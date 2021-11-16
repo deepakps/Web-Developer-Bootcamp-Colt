5b18eea02c1f18c96604c334d8ed09ee64da8445
Deepak Shinde <deepak27shinde@gmail.com>
2021-11-16 17:30:31 +0530

    Sec 50: End of Auth from scratch.

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'notagoodsecret' }));
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('This is the Home Page!');
 });
@@ -31,21 +38,19 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect('/login');
-    } else {
-        res.send('This is secret! You cannot see me unless you are logged!');
-    }
+app.get('/secret', requireLogin, (req, res) => {
+
+    res.render('secret');
 });
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('Top Secret');
+})
 
 app.post('/register', async(req, res) => {
     const { username, password } = req.body;
     const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    });
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -57,15 +62,20 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async(req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validUser = await bcrypt.compare(password, user.password);
+    const foundUser = await User.findAndValidate(username, password);
 
-    if (validUser) {
-        req.session.user_id = user._id;
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret');
     } else {
         res.send(`Please try again ${user.username}!`);
     }
+});
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('/login');
 });
 
 app.listen(3000, () => {
