c89f1f3f3a72034c84b917fe0f6209e2921d6b01
Deepak Shinde <deepak27shinde@gmail.com>
2021-11-19 15:50:04 +0530

    Section 51: Login Routes.

--- Node JS/YelpCamp/routes/users.js
+++ Node JS/YelpCamp/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const asyncErrorCatch = require('../utils/asyncErrorCatch');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -20,5 +21,14 @@ router.post('/register', asyncErrorCatch(async(req, res) => {
         res.redirect('/register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!');
+    res.redirect('/campgrounds');
+});
 
 module.exports = router;
