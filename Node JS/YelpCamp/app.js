abf5c258d7e77cefa1fe1eae5a070b5a1dd8d33e
Deepak Shinde <deepak27shinde@gmail.com>
2021-12-31 18:00:30 +0530

    Section 51 : Fixing Register route.

--- Node JS/YelpCamp/app.js
+++ Node JS/YelpCamp/app.js
@@ -66,6 +66,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    // console.log(res.locals.currentUser);
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
